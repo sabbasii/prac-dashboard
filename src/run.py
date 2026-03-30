1181ae370b6474c7236efd0b8fd3fd1634fc94e3
sabbasii <siimaabbasii@gmail.com>
2026-03-30 17:24:15 -0600

    Update Streamlit dashboard with authentication UI and basic analytics components

--- src/run.py
+++ src/run.py
@@ -1,27 +1,42 @@
+from matplotlib.pyplot import subplots
 import streamlit as st
 import json
+import seaborn as sns
+import numpy as np
+import matplotlib.pyplot as plt
 
-try:
-    from StringIO import StringIO
-except ImportError:
-    from io import StringIO
 
+login_options = st.sidebar.radio('Login/Sign Up', ['Login', 'Sign Up'])
+
+
+if login_options == 'Login':
+    st.sidebar.write('Please enter your login credentials.')
+    st.sidebar.text_input('Username:')
+    st.sidebar.text_input('Password:', type='password')
+    st.sidebar.button('Login')
+else: 
+    st.sidebar.write('Please enter your details to sign up.')
+    st.sidebar.text_input('Username:')
+    st.sidebar.text_input('Email:')
+    st.sidebar.text_input('Password:', type='password')
+    st.sidebar.button('Sign Up')
+
+# Banner and title
+st.image('data/banner.png')
 st.title(':zap: Test Dashboard')
 
+# Metrics
+st.metric('Total Users', '1,234', '5% increase')
+
+# Statistics
 with st.expander('Statistics'):
-    uploaded_file = st.file_uploader("Choose a file")
-    if uploaded_file is not None:
-        # To read file as bytes:
-        bytes_data = uploaded_file.getvalue()
-        st.write(bytes_data)
-
-        # To convert to a string based IO:
-        stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-        st.write(stringio)
-
-        # To read file as string:
-        string_data = stringio.read()
-        st.write(string_data)
-        
-        data = json.loads(string_data)
-        st.json(data)
+    fig, ax = plt.subplots(1, 1, figsize=(10, 5))
+    sns.histplot(np.random.normal(size=100), ax=ax)
+    st.pyplot(fig)
+    
+# User info
+with st.expander('User Profile'):
+    col1, col2 = st.columns(2)
+    col1.text_input('Name:')
+    col2.text_input('Email:')
+    st.camera_input('Take a picture:')
